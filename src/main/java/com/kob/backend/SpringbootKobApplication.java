79187f237cbf1893d3a4145c565c061a3d522aea
“zby” <406735442@qq.com>
2023-07-01 21:48:14 +0800

    7.1

---
+++ src/main/java/com/kob/backend/SpringbootKobApplication.java
@@ -0,0 +1,13 @@
+package com.kob.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKobApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kob/backend/controller/pk.java
@@ -0,0 +1,34 @@
+package com.kob.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+@RestController
+@RequestMapping("/pk/")
+public class pk {
+    @RequestMapping("/index")
+    public List<Map<String,String>> getInfo(){
+        List<Map<String,String>> list = new LinkedList<>();
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("rating","1500");
+        Map<String,String> bot2 = new HashMap<>();
+        bot2.put("name","triger");
+        bot2.put("rating","1500");
+        list.add(bot1);
+        //list.add(bot2);
+        return list;
+    }
+    @RequestMapping("/getinfo")
+    public Map<String,String> GetInfomation(){
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("rating","1500");
+        return bot1;
+    }
+}
