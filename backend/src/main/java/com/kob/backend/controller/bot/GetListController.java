71024e5a7c09259ecc8811075dd7c2438523d560
“zby” <406735442@qq.com>
2023-09-12 18:46:23 +0800

    bot项目

---
+++ backend/src/main/java/com/kob/backend/controller/bot/GetListController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.bot;
+
+import com.kob.backend.Pojo.Bot;
+import com.kob.backend.Service.Impl.User.bot.GetListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GetListController {
+    @Autowired
+    private GetListService getListService;
+
+    @RequestMapping("")
+    public List<Bot> getList (Map<String,String> data){
+        return getListService.getBotList(data);
+    }
+}
---
+++ web/src/views/user/bot/UserbotIndexView.vue
@@ -0,0 +1,21 @@
+<template>
+    <ContentFiled>
+        用户bot
+    </ContentFiled>
+</template>
+
+<script>
+import ContentFiled from '../../../components/ContentFiled.vue'
+
+export default {
+  components: { ContentFiled},
+    compoents:{
+        ContentFiled
+    }
+
+}
+</script>
+
+<style>
+
+</style>
